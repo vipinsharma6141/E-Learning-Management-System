a45b24f4a0030108efc8b036ab6b727c1b955105
vipinsharma6141 <122618858+vipinsharma6141@users.noreply.github.com>
2025-07-15 01:14:59 +0530

    Starting frontend

---
+++ frontend/src/App.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/home/Home";
+import Header from "./components/header/Header";
+
+const App = () => {
+  return (
+    <>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
+};
+
+export default App;
--- server/index.js
+++ server/index.js
@@ -2,6 +2,7 @@ import express from "express";
 import dotenv from "dotenv";
 import { connectDb } from "./database/db.js";
 import Razorpay from "razorpay";
+import cors from "cors";
 
 dotenv.config();
 
@@ -14,6 +15,7 @@ const app = express();
 
 // using middlewares
 app.use(express.json());
+app.use(cors());
 
 const port = process.env.PORT;
 
